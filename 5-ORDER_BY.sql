ea1ff9535065cecfd53142d150700a6b6b03d231
tornadoblot <newclearplant@gmail.com>
2022-12-18 18:10:16 +0900

    5강 ORDER BY

---
+++ 5-ORDER_BY.sql
@@ -0,0 +1,12 @@
+
+SELECT TOP 1 PERCENT *
+FROM players
+WHERE birthYear IS NOT NULL
+ORDER BY birthYear DESC, birthMonth DESC, birthDay DESC;
+
+
+SELECT *
+FROM players
+WHERE birthYear IS NOT NULL
+ORDER BY birthYear DESC, birthMonth DESC, birthDay DESC
+OFFSET 100 ROWS FETCH NEXT 100 ROWS ONLY;
